ab7d7b50c971a193d6b0b1834b6815289b2ccece
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-01 10:23:47 -0500

    DEV:LOGIN Completed login. Returns a token if valid credentials are entered

--- routers/userAuthRouter.js
+++ routers/userAuthRouter.js
@@ -33,4 +33,69 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    if (email && password) {
+      console.log(`:: LOGIN :: USER NAME AND PASSWORD CAPTURED SUCCESSFULLY::`);
+      let user = await User.findBy({ email });
+      console.log(`:: LOGIN :: USER IS FOUND :: ${user}`);
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        console.log(`:: LOGIN :: TOKEN GENERATED ::}`);
+        res.status(200).json({
+          message: `${user.first_name} is successfully logged in`,
+          token,
+          user: user
+        });
+      } else {
+        res
+          .status(401)
+          .json({ message: "Sorry, incorrect email or password." });
+      }
+    } else {
+      res.status(400).json({
+        message: `Submit both an email and password when registering`
+      });
+    }
+  } catch (error) {
+    console.log(`:: LOGIN :: ERROR :: IS ${error}`);
+    res
+      .status(500)
+      .json({ message: "Sorry, but something went wrong while logging in." });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  console.log(`:: LOGGING OUT ::`);
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res
+          .status(400)
+          .json({ message: `There was an error logging out the user.` });
+        console.log(`:: LOGOUT :: ERROR IS :: ${err}`);
+      } else {
+        console.log(`:: LOGOUT :: LOGOUT SUCCESSFUL ::`);
+        res.status(200).json({ message: "Logout successful!" });
+      }
+    });
+  } else {
+    res.status(400).json({ message: "you were never here to begin with" });
+  }
+});
+
+function generateToken(user) {
+  const payLoad = {
+    subject: user.id,
+    email: user.email
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payLoad, jwtKey.jwtSecret, options);
+}
+
 module.exports = router;
