a14f6e69e035582cdc219b36eec97ed17c8ce6ff
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-01 09:24:16 -0500

    DEV:BUGFIX Getting heroku app up and running

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const server = require("./api/server");
 
 const port = process.env.PORT || 5000;
+console.log("---------PORT IS ---------" + port);
 
 server.listen(port, () =>
   console.log(`\n ** Server is runningin on port ${port} **\n`)
